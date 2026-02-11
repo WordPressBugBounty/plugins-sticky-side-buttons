09ac9a5baa8c40dfb4f262d05c89f003ca9b492e
Sean Murphy <sean@iamseanmurphy.com>
2026-02-11 04:52:09 +0000

    2.0.3

--- sticky-side-buttons/sticky-side-buttons.php
+++ sticky-side-buttons/sticky-side-buttons.php
@@ -11,7 +11,7 @@ Domain Path: /languages
 License: GPL v3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 Requires at least: 5.0
-Tested up to: 6.4
+Tested up to: 6.7
 Requires PHP: 7.4
 Network: false
 */
